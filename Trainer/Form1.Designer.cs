ddee285f3f40588e2cdf22ddc95baec35d88ea16
andre <andre@DESKTOP-D6OJM3V>
2022-08-28 12:47:53 +0300

    Add project files.

---
+++ Trainer/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿using System.Drawing;
+
+namespace Trainer
+{
+    partial class Trainer_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Infinite_Ammo = new System.Windows.Forms.CheckBox();
+            this.Infinite_Health = new System.Windows.Forms.CheckBox();
+            this.Jump = new System.Windows.Forms.CheckBox();
+            this.No_Damage = new System.Windows.Forms.CheckBox();
+            this.trackBar_Jump = new System.Windows.Forms.TrackBar();
+            this.BGWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProcOpenLabel = new System.Windows.Forms.Label();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Jump)).BeginInit();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Infinite_Ammo
+            // 
+            this.Infinite_Ammo.AutoSize = true;
+            this.Infinite_Ammo.Location = new System.Drawing.Point(20, 47);
+            this.Infinite_Ammo.Margin = new System.Windows.Forms.Padding(4);
+            this.Infinite_Ammo.Name = "Infinite_Ammo";
+            this.Infinite_Ammo.Size = new System.Drawing.Size(118, 20);
+            this.Infinite_Ammo.TabIndex = 0;
+            this.Infinite_Ammo.Text = "Infinite Ammo";
+            this.Infinite_Ammo.UseVisualStyleBackColor = true;
+            // 
+            // Infinite_Health
+            // 
+            this.Infinite_Health.AutoSize = true;
+            this.Infinite_Health.BackColor = System.Drawing.Color.DimGray;
+            this.Infinite_Health.Location = new System.Drawing.Point(20, 19);
+            this.Infinite_Health.Margin = new System.Windows.Forms.Padding(4);
+            this.Infinite_Health.Name = "Infinite_Health";
+            this.Infinite_Health.Size = new System.Drawing.Size(120, 20);
+            this.Infinite_Health.TabIndex = 1;
+            this.Infinite_Health.Text = "Infinite Health";
+            this.Infinite_Health.UseVisualStyleBackColor = false;
+            // 
+            // Jump
+            // 
+            this.Jump.AutoSize = true;
+            this.Jump.Location = new System.Drawing.Point(20, 115);
+            this.Jump.Margin = new System.Windows.Forms.Padding(4);
+            this.Jump.Name = "Jump";
+            this.Jump.Size = new System.Drawing.Size(63, 20);
+            this.Jump.TabIndex = 2;
+            this.Jump.Text = "Jump";
+            this.Jump.UseVisualStyleBackColor = true;
+            this.Jump.CheckedChanged += new System.EventHandler(this.Jump_CheckedChanged);
+            // 
+            // No_Damage
+            // 
+            this.No_Damage.AutoSize = true;
+            this.No_Damage.Location = new System.Drawing.Point(20, 76);
+            this.No_Damage.Margin = new System.Windows.Forms.Padding(4);
+            this.No_Damage.Name = "No_Damage";
+            this.No_Damage.Size = new System.Drawing.Size(109, 20);
+            this.No_Damage.TabIndex = 3;
+            this.No_Damage.Text = "No Damage";
+            this.No_Damage.UseVisualStyleBackColor = true;
+            this.No_Damage.CheckedChanged += new System.EventHandler(this.No_Damage_CheckedChanged);
+            // 
+            // trackBar_Jump
+            // 
+            this.trackBar_Jump.Location = new System.Drawing.Point(104, 111);
+            this.trackBar_Jump.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar_Jump.Maximum = 5;
+            this.trackBar_Jump.Minimum = 1;
+            this.trackBar_Jump.Name = "trackBar_Jump";
+            this.trackBar_Jump.Size = new System.Drawing.Size(156, 45);
+            this.trackBar_Jump.TabIndex = 4;
+            this.trackBar_Jump.Value = 1;
+            // 
+            // BGWorker
+            // 
+            this.BGWorker.WorkerReportsProgress = true;
+            this.BGWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorker_DoWork);
+            this.BGWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorker_ProgressChanged);
+            this.BGWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorker_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Proccess:";
+            // 
+            // ProcOpenLabel
+            // 
+            this.ProcOpenLabel.AutoSize = true;
+            this.ProcOpenLabel.Location = new System.Drawing.Point(82, 26);
+            this.ProcOpenLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ProcOpenLabel.Name = "ProcOpenLabel";
+            this.ProcOpenLabel.Size = new System.Drawing.Size(33, 16);
+            this.ProcOpenLabel.TabIndex = 6;
+            this.ProcOpenLabel.Text = "N/A";
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.metroPanel1.Controls.Add(this.No_Damage);
+            this.metroPanel1.Controls.Add(this.Jump);
+            this.metroPanel1.Controls.Add(this.trackBar_Jump);
+            this.metroPanel1.Controls.Add(this.Infinite_Health);
+            this.metroPanel1.Controls.Add(this.Infinite_Ammo);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(1, 45);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(330, 199);
+            this.metroPanel1.TabIndex = 7;
+            this.metroPanel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // Trainer_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 258);
+            this.Controls.Add(this.ProcOpenLabel);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DarkCyan;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Trainer_Form";
+            this.Padding = new System.Windows.Forms.Padding(30, 74, 30, 25);
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Trainer_Form_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Jump)).EndInit();
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox Infinite_Ammo;
+        private System.Windows.Forms.CheckBox Infinite_Health;
+        private System.Windows.Forms.CheckBox Jump;
+        private System.Windows.Forms.CheckBox No_Damage;
+        private System.Windows.Forms.TrackBar trackBar_Jump;
+        private System.ComponentModel.BackgroundWorker BGWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ProcOpenLabel;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+    }
+}
+
---
+++ Trainer/Form1.cs
@@ -0,0 +1,212 @@
+﻿using Memory;
+using MetroFramework.Forms;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Threading;
+using System.Windows.Forms;
+
+
+
+
+namespace Trainer
+{
+    public partial class Trainer_Form : MetroForm
+    {
+        Mem mem = new Mem();
+
+        public IntPtr getHandle()
+        {
+            Process[] processes = Process.GetProcessesByName("ac_client");
+            if (processes.Length != 0)
+            {
+                IntPtr pHandle = processes[0].Handle;
+                return pHandle;
+            }
+            return IntPtr.Zero;
+        }
+
+        string ammoAdress = "ac_client.exe+0x00183828,8,190,428";
+        string hpAdress = "ac_client.exe+0x00183828,8,C88,B54";
+        bool ProcOpen = false;
+
+
+        public Trainer_Form()
+        {
+            InitializeComponent();
+            metroPanel1.Enabled = false;
+        }
+        
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool VirtualFreeEx(
+        IntPtr hProcess,
+        UIntPtr lpAddress,
+        UIntPtr dwSize,
+        uint dwFreeType
+        );
+
+        string FloatToHex(float f)
+        {
+            var bytes = BitConverter.GetBytes(f);
+            var i = BitConverter.ToInt32(bytes, 0);
+            return i.ToString("X8");
+        }
+
+
+        public static byte[] HexToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+
+        public byte[] ModifValue(int TrackBar)
+        {
+            var OriginalValue = FloatToHex((float)TrackBar);
+            var ReverseHex = new SoapHexBinary(SoapHexBinary.Parse(OriginalValue).Value.Reverse().ToArray()).ToString();
+            var HexToBArray = HexToByteArray(ReverseHex);
+            return HexToBArray;
+        }
+
+
+        public class Globals
+        {
+            public UIntPtr codecavebase;
+            public byte[] originalBytes_jump = { 0xC7, 0x46, 0x18, 0x00, 0x00, 0x00, 0x40 };
+            public byte[] originalBytes_damage = { 0x89, 0x82, 0xEC, 0x00, 0x00, 0x00 };
+            public byte[] newBytes_jump = { 0xC7, 0x46, 0x18, 0x00, 0x00, 0x00, 0x40 };
+            public byte[] newBytes_damage = { 0xB8, 0xF4, 0x01, 0x00, 0x00, 0x89, 0x82, 0xEC, 0x00, 0x00, 0x00 };
+
+
+            public UIntPtr Codecavebase { get => codecavebase; set => codecavebase = value; }
+            public byte[] NewBytes_jump { get => newBytes_jump; set => newBytes_jump = value; }
+            public byte[] NewBytes_damage { get => newBytes_damage; set => newBytes_damage = value; }
+            public byte[] OriginalBytes_jump { get => originalBytes_jump; }
+            public byte[] OriginalBytes_damage { get => originalBytes_damage; }
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar_Jump.ValueChanged += trackBar_Jump_ValueChanged;
+        }
+
+        private void BGWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            ProcOpen = mem.OpenProcess("ac_client");
+            if (!ProcOpen)
+            {
+                Thread.Sleep(100);
+                return;
+            }
+
+            Thread.Sleep(100);
+            BGWorker.ReportProgress(0);
+
+        }
+
+        private void Trainer_Form_Shown(object sender, EventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
+        private void BGWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            if (ProcOpen)
+            {
+                metroPanel1.Enabled = true;
+                ProcOpenLabel.ForeColor = System.Drawing.Color.Green;
+                ProcOpenLabel.Text = "Game Found";
+            }
+
+            if (Infinite_Ammo.Checked)
+            {
+                mem.WriteMemory(ammoAdress, "int", "9999");
+            }
+
+            if (Infinite_Health.Checked)
+            {
+                mem.WriteMemory(hpAdress, "int", "1000");
+            }
+        }
+
+        private void BGWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (!ProcOpen)
+            {
+                metroPanel1.Enabled = false;
+                ProcOpenLabel.ForeColor = System.Drawing.Color.Red;
+                ProcOpenLabel.Text = "Game Not Found";
+            }
+
+            BGWorker.RunWorkerAsync();
+        }
+
+        private void trackBar_Jump_ValueChanged(object sender, System.EventArgs e)
+        {
+            var Modif_Jump = ModifValue(trackBar_Jump.Value);
+            jump.NewBytes_jump = new byte[] { 0xC7, 0x46, 0x18, Modif_Jump[0], Modif_Jump[1], Modif_Jump[2], Modif_Jump[3] };
+
+            mem.WriteBytes("ac_client.exe+C2486", jump.originalBytes_jump);
+            VirtualFreeEx(getHandle(), (UIntPtr)jump.Codecavebase, (UIntPtr)0, 0x8000);
+
+            if (Jump.Checked == true)
+            {
+                jump.Codecavebase = mem.CreateCodeCave("ac_client.exe+C2486", jump.newBytes_jump, 7, 1000);
+            }
+
+            if (Jump.Checked == false)
+            {
+                mem.WriteBytes("ac_client.exe+C2486", jump.originalBytes_jump);
+                VirtualFreeEx(getHandle(), (UIntPtr)jump.Codecavebase, (UIntPtr)0, 0x8000);
+            }
+
+        }
+
+        private void Jump_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Jump.Checked == true)
+            {
+                jump.Codecavebase = mem.CreateCodeCave("ac_client.exe+C2486", jump.newBytes_jump, 7, 1000);
+            }
+
+            if (Jump.Checked == false)
+            {
+                mem.WriteBytes("ac_client.exe+C2486", jump.originalBytes_jump);
+                VirtualFreeEx(getHandle(), (UIntPtr)jump.Codecavebase, (UIntPtr)0, 0x8000);
+            }
+        }
+
+        private void No_Damage_CheckedChanged(object sender, EventArgs e)
+        {
+            if (No_Damage.Checked == true)
+            {
+                damage.Codecavebase = mem.CreateCodeCave("ac_client.exe+84499", damage.NewBytes_damage, 6, 1000);
+            }
+
+
+            if (No_Damage.Checked == false)
+            {
+                mem.WriteBytes("ac_client.exe+84499", damage.originalBytes_damage);
+                VirtualFreeEx(getHandle(), (UIntPtr)damage.Codecavebase, (UIntPtr)0, 0x8000);
+            }
+        }
+
+
+
+
+        Globals jump = new Globals();
+        Globals damage = new Globals();
+
+
+
+    }
+}
